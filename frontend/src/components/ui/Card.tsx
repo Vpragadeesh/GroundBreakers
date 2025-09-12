c6679d500f251e8fbae7445128dde0de6ff298d9
Vpragadeesh <vpragadeesh.aiml2023@citchennai.net>
2025-09-12 22:00:15 +0530

    [Update] tmp -> frontend

--- frontend/src/components/ui/Card.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Card: React.FC<{ children: React.ReactNode; className?: string }> = ({ children, className = '' }) => (
-  <div className={`p-4 bg-white rounded shadow ${className}`}>{children}</div>
-)
-
-export default Card
